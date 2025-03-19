34c3a14a72ea3cae803a851a5c5cadcd5ca8bfa9
avrjakhov <a.v.rjakhov@yandex.ru>
2025-03-20 04:18:56 +0500

    feat[ter-homeworks-02]: add task4 solution

--- ter-homeworks/02/src/outputs.tf
+++ ter-homeworks/02/src/outputs.tf
@@ -2,6 +2,17 @@ output "service_account_name" {
   value = yandex_iam_service_account.sa.name
 }
 
-output "vm_public_ip" {
-  value = yandex_compute_instance.platform.network_interface.0.nat_ip_address
+output "vm_info" {
+  value = jsonencode({
+    platform = {
+      instance_name = yandex_compute_instance.platform.name
+      external_ip   = yandex_compute_instance.platform.network_interface[0].nat_ip_address
+      fqdn          = yandex_compute_instance.platform.fqdn
+    },
+    platform_db = {
+      instance_name = yandex_compute_instance.platform_db.name
+      external_ip   = yandex_compute_instance.platform_db.network_interface[0].nat_ip_address
+      fqdn          = yandex_compute_instance.platform_db.fqdn
+    }
+  })
 }
