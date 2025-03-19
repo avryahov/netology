74bf41b85aa39f08a3720829bf674321eb5c17d3
avrjakhov <a.v.rjakhov@yandex.ru>
2025-03-20 02:14:23 +0500

    feat[ter-homeworks-02]: set value to vms_ssh_root_key into bash

--- ter-homeworks/02/src/generate_tfvars.sh
+++ ter-homeworks/02/src/generate_tfvars.sh
@@ -12,12 +12,16 @@ CLOUD_ID=$(yc config get cloud-id)
 # Получение folder_id
 FOLDER_ID=$(yc config get folder-id)
 
+# Получение ssh-key
+SSH_KEY=$(cat "$HOME"/.ssh/id_ed25519.pub)
+
 # Запись значений в файл terraform.tfvars
 cat <<EOF > terraform.tfvars
 token = "$TOKEN"
 organization_id = "$ORGANIZATION_ID"
 cloud_id  = "$CLOUD_ID"
 folder_id = "$FOLDER_ID"
+vms_ssh_root_key = "$SSH_KEY"
 EOF
 
 echo "Файл terraform.tfvars успешно создан."
