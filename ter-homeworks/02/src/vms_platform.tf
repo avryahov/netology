a7f0654925bb1e204bee4671c9382cd3a9cb1ff3
avrjakhov <a.v.rjakhov@yandex.ru>
2025-03-20 03:32:56 +0500

    feat[ter-homeworks-02]: add vm db vars

--- ter-homeworks/02/src/vms_platform.tf
+++ ter-homeworks/02/src/vms_platform.tf
@@ -59,6 +59,73 @@ variable "vm_web_metadata_enable_oslogin" {
   default     = "true"
 }
 
+### vm db vars
+variable "vm_db_family" {
+  description = "The family of yandex compute images"
+  type        = string
+  default     = "ubuntu-2004-lts"
+}
+
+variable "vm_db_name" {
+  description = "The name of the yandex compute instance"
+  type        = string
+  default     = "netology-develop-platform-db"
+}
+
+variable "vm_db_platform_id" {
+  description = "The platform identifier of the yandex compute instance"
+  type        = string
+  default     = "standard-v1"
+}
+
+variable "vm_db_zone" {
+  description = "The zone of the yandex compute instance"
+  type        = string
+  default     = "ru-central1-b"
+}
+
+variable "vm_db_resources_cores" {
+  description = "The cores value of the yandex compute instance resources"
+  type        = number
+  default     = 2
+}
+
+variable "vm_db_resources_memory" {
+  description = "The memory value of the yandex compute instance resources"
+  type        = number
+  default     = 2
+}
+
+variable "vm_db_resources_core_fraction" {
+  description = "The core fraction value of the yandex compute instance resources"
+  type        = number
+  default     = 20
+}
+
+variable "vm_db_scheduling_policy_preemptible" {
+  description = "The preemptible flag of the yandex compute instance scheduling policy"
+  type        = bool
+  default     = true
+}
+
+variable "vm_db_network_interface_nat" {
+  description = "The nat flag of the yandex compute instance network interface"
+  type        = bool
+  default     = true
+}
+
+variable "vm_db_metadata_serial_port_enable" {
+  description = "The serial port enable value of the instance metadata"
+  type        = number
+  default     = 1
+}
+
+variable "vm_db_metadata_enable_oslogin" {
+  description = "The enable os login flag of the instance metadata"
+  type        = string
+  default     = "true"
+}
+
 ### ssh vars
 
 variable "vms_ssh_root_key" {
