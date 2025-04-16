09500f9731045425c3ed872d88d998bf9ceb1b7a
avrjakhov <a.v.rjakhov@yandex.ru>
2025-04-16 23:19:55 +0500

    feat[ter-homeworks-03]: add source

--- ter-homeworks/03/src/providers.tf
+++ ter-homeworks/03/src/providers.tf
@@ -4,7 +4,7 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
-  required_version = ">=1.5"
+  required_version = "~>1.8.4"
 }
 
 provider "yandex" {
@@ -12,6 +12,4 @@ provider "yandex" {
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = var.default_zone
-
-  # service_account_key_file = file(".authorized_key.json")
 }
